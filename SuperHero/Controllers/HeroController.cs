d269697fc2943f87e33f0b7d39b0656cfd5b4cff
eddyiiz <eddyxsayf@gmail.com>
2024-07-06 17:44:34 +0200

    added all API endpoint

--- SuperHero/Controllers/HeroController.cs
+++ SuperHero/Controllers/HeroController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
 namespace SuperHero.Controllers;
 
 [Route("api/[controller]")]
@@ -17,6 +16,38 @@ public class HeroController : ControllerBase
     public async Task<ActionResult<ServiceResponse<List<Hero>>>> GetHeroes()
     {
         var result = await _heroService.GetHeroes();
+
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ServiceResponse<Hero>>> GetHero(int id)
+    {
+        var result = await _heroService.GetHero(id);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ServiceResponse<Hero>>> CreateHero(Hero hero)
+    {
+        var result = await _heroService.CreateHero(hero);
+
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ServiceResponse<Hero>>> UpdateHero(int id, Hero newHero)
+    {
+        var result = await _heroService.UpdateHero(id, newHero);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ServiceResponse<List<Hero>>>> DeleteHero(int id)
+    {
+        var result = await _heroService.DeleteHero(id);
 
         return Ok(result);
     }
