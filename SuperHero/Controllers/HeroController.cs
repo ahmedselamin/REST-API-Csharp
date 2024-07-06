dc20b6b3e4ad4d84179bc9ccba469fcf1507e4a6
eddyiiz <eddyxsayf@gmail.com>
2024-07-06 17:26:17 +0200

    code first migration

---
+++ SuperHero/Controllers/HeroController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHero.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HeroController : ControllerBase
+{
+    private readonly IHeroService _heroService;
+
+    public HeroController(IHeroService heroService)
+    {
+        _heroService = heroService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ServiceResponse<List<Hero>>>> GetHeroes()
+    {
+        var result = await _heroService.GetHeroes();
+
+        return Ok(result);
+    }
+}
