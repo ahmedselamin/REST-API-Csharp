2376c8d05a2a0d5a75df69dcb779011fa634eb53
eddyiiz <eddyxsayf@gmail.com>
2024-07-06 17:18:45 +0200

    Added IHeroService and implementaion

--- SuperHero/Services/HeroService/HeroService.cs
+++ SuperHero/Services/HeroService/HeroService.cs
@@ -1,43 +1,86 @@
 ﻿
-namespace SuperHero.Services.HeroService
+namespace SuperHero.Services.HeroService;
+
+public class HeroService : IHeroService
 {
-    public class HeroService : IHeroService
+    private readonly DataContext _context;
+
+    public HeroService(DataContext context)
     {
-        private readonly DataContext _context;
-
-        public HeroService(DataContext context)
-        {
-            _context = context;
-        }
-        public async Task<ServiceResponse<List<Hero>>> GetHeroes()
-        {
-            var response = new ServiceResponse<List<Hero>>
-            {
-                Data = await _context.Heroes.ToListAsync()
-            };
-
-            return response;
-        }
-        public Task<ServiceResponse<Hero>> CreateHero(Hero hero)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ServiceResponse<bool>> DeleteHero(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ServiceResponse<Hero>> GetHero(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-
-
-        public Task<ServiceResponse<Hero>> UpdateHero(Hero hero)
-        {
-            throw new NotImplementedException();
-        }
+        _context = context;
     }
+    public async Task<ServiceResponse<List<Hero>>> GetHeroes()
+    {
+        var response = new ServiceResponse<List<Hero>>
+        {
+            Data = await _context.Heroes.ToListAsync()
+        };
+
+        return response;
+    }
+
+    public async Task<ServiceResponse<Hero>> GetHero(int id)
+    {
+        var response = new ServiceResponse<Hero>();
+        var hero = await _context.Heroes.FindAsync(id);
+
+        if (hero == null)
+        {
+            response.Success = false;
+            response.Message = "Not found";
+        }
+
+        response.Data = hero;
+
+        return response;
+    }
+    public async Task<ServiceResponse<List<Hero>>> CreateHero(Hero hero)
+    {
+        var response = new ServiceResponse<List<Hero>>();
+
+        _context.Heroes.Add(hero);
+        await _context.SaveChangesAsync();
+        response.Data = await _context.Heroes.ToListAsync();
+
+        return response;
+    }
+
+    public async Task<ServiceResponse<Hero>> UpdateHero(int id, Hero UpdatedHero)
+    {
+        var response = new ServiceResponse<Hero>();
+        var hero = await _context.Heroes.FindAsync(id);
+
+        if (hero == null)
+        {
+            response.Success = false;
+            response.Message = "not found";
+        }
+
+        hero.Name = UpdatedHero.Name;
+        hero.SuperPower = UpdatedHero.SuperPower;
+
+        await _context.SaveChangesAsync();
+        response.Data = hero;
+
+        return response;
+    }
+
+    public async Task<ServiceResponse<List<Hero>>> DeleteHero(int id)
+    {
+        var response = new ServiceResponse<List<Hero>>();
+        var hero = await _context.Heroes.FindAsync(id);
+
+        if (hero == null)
+        {
+            response.Success = false;
+            response.Message = "not found";
+        }
+
+        _context.Heroes.Remove(hero);
+        await _context.SaveChangesAsync();
+        response.Data = await _context.Heroes.ToListAsync();
+
+        return response;
+    }
+
 }
--- SuperHero/Services/HeroService/IHeroService.cs
+++ SuperHero/Services/HeroService/IHeroService.cs
@@ -4,7 +4,7 @@ public interface IHeroService
 {
     Task<ServiceResponse<List<Hero>>> GetHeroes();
     Task<ServiceResponse<Hero>> GetHero(int id);
-    Task<ServiceResponse<Hero>> CreateHero(Hero hero);
-    Task<ServiceResponse<Hero>> UpdateHero(Hero hero);
-    Task<ServiceResponse<bool>> DeleteHero(int id);
+    Task<ServiceResponse<List<Hero>>> CreateHero(Hero hero);
+    Task<ServiceResponse<Hero>> UpdateHero(int id, Hero UpdatedHero);
+    Task<ServiceResponse<List<Hero>>> DeleteHero(int id);
 }
